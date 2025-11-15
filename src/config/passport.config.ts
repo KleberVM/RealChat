8cb4e8084659d7c8bfe4619546f868b70050a0e1
Kleber <muruchi2018@gmail.com>
2025-11-15 10:50:15 -0400

    configuracion de passport url rollback

--- src/config/passport.config.ts
+++ src/config/passport.config.ts
@@ -7,7 +7,7 @@ dotenv.config();
 passport.use(new GoogleStrategy({
     clientID: String(process.env.GOOGLE_CLIENT_ID),
     clientSecret: String(process.env.GOOGLE_CLIENT_SECRET),
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || "http://localhost:3000/api/auth/google/callback"
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
